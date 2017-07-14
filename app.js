1de0fd94b47be45f90ee7f72378515b0e76353d9
Bohdan-Ivan Maliuha <bogdan.maliuga@gmail.com>
2017-07-14 17:05:27 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Activity = require('./models/activity');
+
+
+
+
+// app.use('/api',router);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.get('/',function(req,res) {
+  res.json({message:"Марік мути апку,СУКА"});
+
+})
+ app.listen(9000);
+ console.log('starting application. Good job');
