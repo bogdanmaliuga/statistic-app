eb4eb694988ddf2457f1490dd01ecbb61b8b96f2
Bohdan-Ivan Maliuha <bogdan.maliuga@gmail.com>
2017-08-04 17:24:27 +0300

    api update

--- app.js
+++ app.js
@@ -30,8 +30,7 @@ router.get('/', function(req, res) {
 
 // more routes for our API will happen here
 router.route('/bears')
-
-// create a bear (accessed at POST http://localhost:8080/api/bears)
+//post
     .post(function(req, res) {
 
     var bear = new Bear(); // create a new instance of the Bear model
@@ -45,14 +44,46 @@ router.route('/bears')
         res.json({message: 'Bear created!'});
     });
 
-})
-// get all the bears (accessed at GET http://localhost:8080/api/bears)
-    .get(function(req, res) {
+}).get(function(req, res) {
     Bear.find(function(err, bears) {
         if (err)
             res.send(err);
 
         res.json(bears);
+    });
+});
+router.route('/bears/:bear_id').get(function(req, res) {
+    Bear.findById(req.params.bear_id, function(err, bear) {
+        if (err)
+            res.send(err);
+        res.json(bear);
+    });
+}).put(function(req, res) {
+
+    Bear.findById(req.params.bear_id, function(err, bear) {
+
+        if (err)
+            res.send(err);
+
+        bear.name = req.body.name; // update the bears info
+
+        // save the bear
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({message: 'Bear updated!'});
+        });
+
+    });
+}).delete(function(req, res) {
+    Bear.remove({
+        _id: req.params.bear_id
+    }, function(err, bear) {
+        if (err)
+            res.send(err);
+
+        res.json({message: 'Successfully deleted'});
     });
 });
 
